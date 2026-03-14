ed4b4c17a10bbf74952aea956bad1043a0029d77
Wannes Paesschesoone <wannespaesschesoone@gmail.com>
2026-03-14 23:59:13 +0100

    implemented databse connection

--- src/database.py
+++ src/database.py
@@ -1,11 +1,29 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
-load_dotenv()  # This loads variables from .env into os.environ
+load_dotenv()
 
 POSTGRES_URL = os.getenv('POSTGRES_URL')
 ASYNC_URL = POSTGRES_URL.replace('postgresql://', 'postgresql+psycopg://')
 
-async_engine = create_async_engine(ASYNC_URL)
+if not ASYNC_URL:
+    raise ValueError('The database URL is not working')
+
+# create_async_engine for async operations
+engine = create_async_engine(ASYNC_URL, echo=False)
+
+# SQLAlchemy 2.0 to handle async sessions
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
+
+
+async def get_db():
+    """Dependency for FastAPI to get a database session."""
+    async with AsyncSessionLocal() as session:
+        yield session
