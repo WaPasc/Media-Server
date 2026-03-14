cbf9a79a9ff8280528fa2c4d25447566b231be9c
Wannes Paesschesoone <wannespaesschesoone@gmail.com>
2026-03-15 00:23:24 +0100

    function create_all must be passed not called

--- src/init_db.py
+++ src/init_db.py
@@ -4,4 +4,4 @@ from db_models import Base
 
 async def init_database() -> None:
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all())
+        await conn.run_sync(Base.metadata.create_all)
