a9bf5e03644e206933718ea15b6ab075ec468e8c
Wannes Paesschesoone <wannespaesschesoone@gmail.com>
2026-03-15 00:29:51 +0100

    execution trigger

--- src/init_db.py
+++ src/init_db.py
@@ -1,7 +1,15 @@
+import asyncio
+
 from database import engine
 from db_models import Base
 
 
 async def init_database() -> None:
+    print('Creating database tables')
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+    print('Tables created succesfully')
+
+
+if __name__ == '__main__':
+    asyncio.run(init_database())
